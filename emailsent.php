aa373f8df960641d85b464f4f7bd5a080fec5538
ujjawala397 <ujjawala397@gmail.com>
2019-05-01 02:32:11 +0530

    Add files via upload

---
+++ emailsent.php
@@ -0,0 +1,25 @@
+<?php
+include('C:/xampp/htdocs/mail/PHPMailerAutoload.php');
+include('C:/xampp/htdocs/mail/class.smtp.php');
+$mail= new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth= true;
+$mail->SMTPSecure='t1s'; //transport layer security protocol
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->Username= 'ujjawala397@gmail.com';
+
+$mail->Password = '17feb1998';
+$mail->Subject = 'hello';
+$mail->Body = 'mail';
+$mail->addAddress('ujjawala397@gmail.com');
+$mail->Send();
+if(!$mail->Send())
+{
+	echo "message not sent";
+}
+else
+{
+	echo "message  sent";
+}
+?>
